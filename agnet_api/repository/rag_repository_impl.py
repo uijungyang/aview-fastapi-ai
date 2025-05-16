261bf1252960d7eab5aee71b67229ef0d580e189
의정 Ui Jung, Yang <5012jung@gmail.com>
2025-05-16 19:22:00 +0900

    feat: [양의정 ] main DB, fallback DB, tech DB RAG 검색 기능 구현 [AIFI-47]

--- agnet_api/repository/rag_repository_impl.py
+++ agnet_api/repository/rag_repository_impl.py
@@ -8,7 +8,9 @@ class RagRepositoryImpl(RagRepository):
     # companyName: 회사이름
     # situation: answerText
 
-    def rag_main(self, companyName: str, situation: str) -> list:
+    def rag_main(self, companyName: str, situation: str, userToken: str) -> list:
+        print(f" AGENT RAG repository - rag_main 실행: {userToken}")
+
         collection = get_chroma_collection(companyName)
         query_embedding = get_embedding(situation)
         result = collection.query(query_embeddings=[query_embedding], n_results=5)
@@ -16,10 +18,27 @@ class RagRepositoryImpl(RagRepository):
         documents = result["documents"][0] if result["documents"] else []
         return documents if documents else ["(메인 회사 DB에서 적절한 질문을 찾지 못했습니다.)"]
 
-    def rag_fallback(self, situation: str) -> list:
+    def rag_fallback(self, situation: str, userToken: str) -> list:
+        print(f" AGENT RAG repository - rag_fallback 실행: {userToken}")
+
         fallback_collection = get_chroma_collection("fallback")
+        print("fallback_collection 찾음")
+        # 이전 답변을 임베딩 -> DB에서 참고해서 질문을 가져올 생각
         query_embedding = get_embedding(situation)
         result = fallback_collection.query(query_embeddings=[query_embedding], n_results=5)
+        print(f"{result}")
+
+        documents = result["documents"][0] if result["documents"] else []
+        return documents if documents else ["(Fallback DB에서도 적절한 질문을 찾지 못했습니다.)"]
+
+    def rag_tech(self, techStack:list[str], situation: str, userToken: str) -> list:
+        print(f" AGENT RAG repository - rag_tech 실행: {userToken}")
+
+        tech_collection = get_chroma_collection("tech_db1")
+        print("tech_db1 찾음")
+        query_embedding = get_embedding(situation)
+        result =  tech_collection.query(query_embeddings=[query_embedding], n_results=5)
+        print(f"{result}")
 
         documents = result["documents"][0] if result["documents"] else []
         return documents if documents else ["(Fallback DB에서도 적절한 질문을 찾지 못했습니다.)"]
